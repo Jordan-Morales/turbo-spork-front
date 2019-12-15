7f99b5b3a8cd0a0b6daec505468c5b4d4b359749
Jordan Morales <jordan.morales@gmail.com>
2019-12-15 12:48:13 -0800

    file restructure

---
+++ build/precache-manifest.54f0e14e798c27a72eb74c87eb26bc55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6990460f61ee810f4dfb3279c6796675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbf94cf3db77f30e8fc7",
+    "url": "/static/css/main.27052747.chunk.css"
+  },
+  {
+    "revision": "0f7dd4d1bf06c62baf0a",
+    "url": "/static/js/2.02e2d6af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.02e2d6af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbf94cf3db77f30e8fc7",
+    "url": "/static/js/main.93446eac.chunk.js"
+  },
+  {
+    "revision": "af53eee2f01cbcc2d96b",
+    "url": "/static/js/runtime-main.d437aa6f.js"
+  }
+]);
