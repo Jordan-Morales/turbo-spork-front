7da5ee2467b7efe941b84d0de3ba501cd7828f57
KB-Z <zdonner179@gmail.com>
2019-12-18 10:22:19 -0500

    attempting to fix non-update on edit.

--- src/App.js
+++ src/App.js
@@ -122,7 +122,8 @@ console.log(modData);
         'Credentials': 'include',
         'Access-Control-Allow-Origin': '*'
   }
-}).then(updatedData => this.handleView('mylaunches'), this.pullStuff())
+}).then(updatedData => {this.handleView('mylaunches')
+this.pullStuff()})
 .catch(err => console.log('this is catch', err))
 }
 
